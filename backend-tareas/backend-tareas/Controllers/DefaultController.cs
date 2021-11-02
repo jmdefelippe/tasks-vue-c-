92a5408bb68b1c62d5ae7edeedbff542ed09aed2
Joan Manuel Defelippe <jmdefelippe@gmail.com>
2021-11-02 12:39:15 -0300

    tasks: vue, net core, entity framework core, sql server

---
+++ backend-tareas/backend-tareas/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend_tareas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : Controller
+    {
+        // GET: api/<DefaultController>
+        [HttpGet]
+        public string Get()
+        {
+            return "Aplicación corriendo...";
+        }
+    }
+}
